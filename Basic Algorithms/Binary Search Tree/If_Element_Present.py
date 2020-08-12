a6cdecbe72fc4ebdee875e1a3d7fccdb1f1d29dc
test_barantsev <anna.tv.sf@gmail.com>
2020-08-12 10:21:19 -0700

    updated binary search "if element present" algorithm

--- Basic Algorithms/Binary Search Tree/If_Element_Present.py
+++ Basic Algorithms/Binary Search Tree/If_Element_Present.py
@@ -1,30 +1,35 @@
 """Given a sorted array, return True if element present, False if not"""
 
-def binary_search(array, num, start, end):
-    mid_point = end // 2
+
+def binary_search(array, num):
+    start = 0
+    end = len(array) - 1
+    mid_point = len(array) // 2
 
     # Base case
     if start == end:
-        if array[start] == num:
+        if array[0] == num:
             return True
         else:
             return False
 
     while start < end:
-        if num == mid_point:
+        if num == array[mid_point]:
             return True
 
-        if num > mid_point:
-            binary_search(array[start:end], num, start=mid_point+1, end=end)
+        if num > array[mid_point]:
+            start = mid_point + 1
+            print(array[start:end+1])
+            print(f'start: {start}, {array[start]}, end: {end}, {array[end]}, mid_point: {mid_point}, {array[mid_point]}')
+            binary_search(array[start:end+1], num)
 
+        if num < array[mid_point]:
+            end = mid_point - 1
+            binary_search(array[start:end], num)
 
-
-
-
-
-
+    return False
 
 
 a = [2, 4, 6, 8, 9, 12, 18]
 
-print(binary_search(a, num=18, start=0, end=len(a)-1))
+print(binary_search(a, 18))
