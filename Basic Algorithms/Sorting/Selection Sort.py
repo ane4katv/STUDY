dbb6b2f09ddc9779fd3bcd9c19489b14922c05e8
test_barantsev <anna.tv.sf@gmail.com>
2020-08-12 18:58:32 -0700

    Added Selection Sort Algorithm

--- Basic Algorithms/Sorting/Selection Sort.py
+++ Basic Algorithms/Sorting/Selection Sort.py
@@ -1,22 +1,22 @@
 def selection_sort(array):
     n = 0
+
     while n < len(array):
         min_elem = array[n]
         min_index = n
         count = n
+
         for i in range(n+1, len(array)):
-            count +=1
+            count += 1
 
             if array[i] < min_elem:
                 min_elem = array[i]
                 min_index = count
+
             if min_elem != array[n]:
                 array[min_index], array[n] = array[n], array[min_index]
 
-        print(array)
-
         n += 1
-
 
     return array
 
