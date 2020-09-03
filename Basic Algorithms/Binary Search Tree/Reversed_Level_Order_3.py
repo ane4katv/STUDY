f14584a4bb67edf39f0b1adc67af7565e250c82b
test_barantsev <anna.tv.sf@gmail.com>
2020-09-02 20:17:46 -0700

    tree_min_depth

--- Basic Algorithms/Binary Search Tree/Reversed_Level_Order_3.py
+++ Basic Algorithms/Binary Search Tree/Reversed_Level_Order_3.py
@@ -1,40 +1,63 @@
 """
-        1
+        5
     /       \
-  2           3
+  3           7
 /   \       /   \
-4   5       6   7
+2   4       6   8
 
-output: [6,7, 4,5, 2,3, 1]
+output: [6,8, 2,4, 3,7, 5]
 
 """
 
 
-class Node:
+class Node(object):
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-    def insert(self, value):
-        if self.value:
-            if value < self.value:
-                if not self.left:
-                    self.left = Node(value)
-                else:
-                    self.left.insert(value)
-            elif value > self.value:
-                if not self.right:
-                    self.right = Node(value)
-                else:
-                    self.right.insert(value)
-        else:
-            self.value = value
-
 
 class BinaryTree:
     def __init__(self, value):
         self.root = Node(value)
+
+    def lst_to_levelorder_lst(self, array):
+        left_part = sorted([array[i] for i in range(len(array)) if array[i] < self.root.value])
+        right_part = sorted([array[i] for i in range(len(array)) if array[i] > self.root.value])
+
+        self.root.left = self.lst_to_bst(left_part)
+        self.root.right = self.lst_to_bst(right_part)
+
+        if not self.root:
+            return
+
+        queue = [self.root]
+        traversal = []
+
+        while len(queue) > 0:
+            traversal.append(queue[0].value)
+            node = queue.pop(0)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+        return traversal
+
+    def lst_to_bst(self, array):
+        if not array:
+            return None
+
+        mid = len(array) // 2
+
+        node = Node(array[mid])
+
+        node.left = self.lst_to_bst(array[:mid])
+        node.right = self.lst_to_bst(array[mid + 1:])
+
+        return node
 
     def reversed_level_trav(self):
         if not self.root:
@@ -67,16 +90,8 @@ class BinaryTree:
         return traversal
 
 
-tree = BinaryTree(1)
-# tree.root.left = Node(2)
-# tree.root.right = Node(3)
-# tree.root.left.left = Node(4)
-# tree.root.left.right = Node(5)
-# tree.root.right.left = Node(6)
-# tree.root.right.right = Node(7)
+tree = BinaryTree(5)
+a = [2, 3, 4, 6, 7, 8]
 
-array = [2, 3, 4, 5, 6, 7]
-for element in array:
-    tree.root.insert(element)
-
+print(tree.lst_to_levelorder_lst(a))
 print(tree.reversed_level_trav())
