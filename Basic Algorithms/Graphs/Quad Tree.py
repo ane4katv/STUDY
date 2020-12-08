bde58d04a69c9bf6b0c707bbdc140be71d3e95b9
test_barantsev <anna.tv.sf@gmail.com>
2020-12-07 19:55:35 -0800

    Quad Tree_corrected_in progress

--- Basic Algorithms/Graphs/Quad Tree.py
+++ Basic Algorithms/Graphs/Quad Tree.py
@@ -9,34 +9,41 @@ class Tree:
     def __init__(self, borders):
         self.main_squad = Quad(borders)
 
-    def divide_to_quads(self, borders, children, points):
-        while len(points) > 4:
+    def divide_to_quads(self, quad):
 
-            mid_x = borders[1][0] / 2
-            mid_y = borders[0][1] / 2
+        while len(quad.points) > 4:
 
-            children.append([[borders[0][0], borders[0][1]], [mid_x, mid_y]])
-            children.append([[mid_x, borders[0][1]], [borders[1][0], mid_y]])
-            children.append([[borders[0][0], mid_y], [mid_x, borders[1][1]]])
-            children.append([[mid_x, borders[1][0]], [borders[1][0], borders[1][1]]])
+            children = []
+
+            mid_x = quad.borders[1][0] / 2
+            mid_y = quad.borders[0][1] / 2
+
+            children.append([[quad.borders[0][0], quad.borders[0][1]], [mid_x, mid_y]])
+            children.append([[mid_x, quad.borders[0][1]], [quad.borders[1][0], mid_y]])
+            children.append([[quad.borders[0][0], mid_y], [mid_x, quad.borders[1][1]]])
+            children.append([[mid_x, quad.borders[1][0]], [quad.borders[1][0], quad.borders[1][1]]])
+
+            for x, y in quad.points:
+                for top_left, bottom_right in children:
+                    if top_left[0] <= x <= bottom_right[0] and top_left[1] <= y <= bottom_right[1]: # how to avoid duplicates here?
+                        kid = Quad([top_left, bottom_right])
+                        kid.points.append([x, y])
+                        self.divide_to_quads(kid)
 
 
-            for top_left, bottom_right in children:
-                kid = Quad([top_left, bottom_right])
-                if points:
-                    for x, y in points:
-                        if top_left[0] <= x <= bottom_right[0] and top_left[1] <= y <= bottom_right[1]:
-                            kid.points.append([x,y])
-                    points = []
-                    self.divide_to_quads(kid.borders, kid.children, kid.points)
-
-
-    def insert(self, point): # point = [x,y]
-
+    def insert(self, quad, point): # point = [x,y]
         # check whether the given node is within the boundaries of the current quad.
-        # If it is within the boundaries, we select the appropriate child to contain this node based on its location.
-        # If not, do not insert
-        pass
+        if quad.borders[0][0] <= point[0] <= quad.borders[1][0] and quad.borders[1][1] <= point[1] <= quad.borders[0][1]:
+            if not quad.children:
+                if len(quad.points) == 4:
+                    self.divide_to_quads(quad)
+                    self.insert(quad, point)
+                else:
+                    quad.points.append(point)
+            else:
+                for kid in quad.children:
+                    self.insert(kid, point)
+
 
     def search(self, point):
         # locate a node in the given quad.
